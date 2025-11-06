e11af692d6eb73088af5a5f47e8f20c7edbd9090
Harsol <hmiranda259@unab.edu.co>
2025-11-05 23:57:32 -0500

    Add files via upload

--- Floristeria/src/Floristeria/Magica.java
+++ Floristeria/src/Floristeria/Magica.java
@@ -1,4 +1,23 @@
 
-public class Magica {
+package Floristeria;
 
+public class Magica extends Casa {
+    private String nombre;
+    private int cantidadFlores;
+
+    public Rossbella(String color, int numHabitaciones, int numBanos, boolean tienePatio, boolean tieneParqueadero,
+                     String nombre, int cantidadFlores) {
+        super(color, numHabitaciones, numBanos, tienePatio, tieneParqueadero);
+        this.nombre = nombre;
+        this.cantidadFlores = cantidadFlores;
+    }
+
+    public String describirFloristeria() {
+        return super.describir() + " La floristería se llama " + nombre +
+               " y tiene " + cantidadFlores + " flores disponibles.";
+    }
+
+    public void pintar(String nuevoColor) {
+        System.out.println("Pintando la floristería con color: " + nuevoColor);
+    }
 }
